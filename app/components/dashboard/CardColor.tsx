b67b3673e42f06bb9118a8dd3dc6184185b1469d
ciamol <tomiivan3@gmail.com>
2026-07-20 16:52:52 -0400

    Add: dashboard components and functionality; implement color extraction and display features

---
+++ app/components/dashboard/CardColor.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const CardColor = () => {
+    return (<div className="bg-white dark:bg-zinc-800 p-4 rounded-lg shadow-md">
+        <div className="w-full h-full flex items-center justify-center p-2">
+            <div className="w-full h-full bg-gray-300 dark:bg-zinc-600 rounded-md"></div>
+        </div>
+    </div>)
+}
--- app/shared/InputFile.tsx
+++ app/shared/InputFile.tsx
@@ -1,12 +1,41 @@
 "use client";
 import { ImagePlus } from "lucide-react";
-import { useRef } from "react";
-export const InputFile = () => {
+import { useRef, useState } from "react";
+interface InputFileProps {
+    handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+export const InputFile:React.FC<InputFileProps> = ({ handleChange }) => {
+    const [ isDragging, setIsDragging ] = useState<boolean>(false);
     const fileInputRef = useRef<HTMLInputElement>(null);
-
     const handleClick = () => {
         fileInputRef.current?.click();
     };
+
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setIsDragging(true);
+    }
+
+    const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setIsDragging(false);
+    }
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setIsDragging(false);
+        if (event.dataTransfer.files && event.dataTransfer.files.length > 0) {
+            if (handleChange) {
+                const fakeEvent = {
+                    target: {
+                        files: event.dataTransfer.files
+                    }
+                } as React.ChangeEvent<HTMLInputElement>;
+                handleChange(fakeEvent);
+            }
+        }
+
+    }
 
     return ( <div className="flex flex-col gap-2 w-full h-full text-gray-700 dark:text-zinc-50">
         <label htmlFor="file-input" className="cursor-pointer text-xs ">
@@ -18,10 +47,20 @@ export const InputFile = () => {
             name="file-input"
             accept="image/*"
             className="hidden"
+            onChange={handleChange}
             ref={fileInputRef}
         />
-        <div className="flex w-full flex-col items-center justify-center w-full h-50 border-2 border-dashed border-gray-300 dark:border-gray-600 rounded-lg p-4 cursor-pointer hover:border-green-500"
+        <div className={`
+                    flex flex-col items-center justify-center p-6 border-2 border-dashed rounded-xl cursor-pointer transition-colors
+                    ${isDragging 
+                        ? "border-green-500 bg-green-50/50 dark:bg-green-950/20" 
+                        : "border-slate-300 dark:border-slate-700 bg-slate-50 dark:bg-slate-900/50 hover:bg-slate-100 dark:hover:bg-slate-800/50"
+                    }
+                `}
             onClick={handleClick}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
         >
             <ImagePlus 
                 width={ 50 }
